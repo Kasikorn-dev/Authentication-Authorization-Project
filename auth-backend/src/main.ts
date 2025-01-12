0be611b4897ff8e59de4231d5ad33a2f81fa0ba3
K <kasikorntripforwork@gmail.com>
2025-01-13 01:30:43 +0700

    fix backend

--- auth-backend/src/main.ts
+++ auth-backend/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.URI, // ระบุ URL ของ Frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   // await app.listen(process.env.PORT ?? 3000);
 
   await app.init();
